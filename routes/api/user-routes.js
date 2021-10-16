fbee58a1c118f3c4e312232b931499e0d7ac5ee1
minhhap <69835497+minhhap@users.noreply.github.com>
2021-10-16 15:25:36 -0700

    built add friend and remove friend routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,9 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/user-controller');
 
 // /api/users
@@ -19,5 +21,11 @@ router
   .get(getUserById)
   .put(updateUser)
   .delete(deleteUser);
+
+// api/users/:id/friends/:friendsId
+router
+  .route('/:id/friends/:friendsId')
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
